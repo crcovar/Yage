5700e0fad16e9cb81569b3d085b71591d34edd3b
Charles Covar <covar1@gmail.com>
2011-10-11 09:41:57 -0400

    added engine.network package, containing 2 classes Client and Server. All networking communication will be handled by these classes and pass data via Events

---
+++ ProcessingEngine/src/engine/network/Client.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package engine.network;
+
+import engine.GameObject;
+
+/**
+ * @author Charles Covar (covar1@gmail.com)
+ *
+ */
+public class Client extends GameObject {
+	public Client() {
+		super();
+	}
+}
