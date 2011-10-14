2366c1e6b899f5ae3dfd8ad464e53fab9b67e9f1
Charles Covar <covar1@gmail.com>
2011-10-14 09:36:07 -0400

    Replay of Level playthrough is shown between levels.

--- ProcessingEngine/src/engine/ProcessingSketch.java
+++ ProcessingEngine/src/engine/ProcessingSketch.java
@@ -3,6 +3,7 @@ import engine.character.Player;
 import engine.events.EventManager;
 import engine.utils.Logger;
 import engine.utils.Recorder;
+import engine.utils.Replay;
 import processing.core.*;
 
 /**
@@ -25,6 +26,7 @@ public class ProcessingSketch extends PApplet {
 		this.logger = new Logger();
 		this.recorder = new Recorder();
 		this.renderer = new Renderer(this);
+		this.replay = null;
 		
 		player = new Player();
 		game = new Game("csc481",player,this);
@@ -69,13 +71,23 @@ public class ProcessingSketch extends PApplet {
 		    currentLevel.movePlayer(Level.RIGHT);
 		}
 		if(checkKey(' ')) currentLevel.movePlayer(Level.UP);
-		  
-		//background(0,0,0);
+		
 		EventManager.getInstance().sendEvent("clear", null);
-		currentLevel.update();
-		if(currentLevel.reachedVictory())
+		if(currentLevel.reachedVictory()) {
+			if(this.replay == null)
+				this.replay = new Replay("replay" + this.recorder.gUId);
+			if(!this.replay.isDone()) {
+				fill(255,255,255);
+				text("REPLAY",10,20);
+				// update and draw the replay
+				this.replay.update();
+				this.replay.draw();
+			} else
 			currentLevel = game.nextLevel();
-		currentLevel.draw();
+		} else {
+			currentLevel.update();
+			currentLevel.draw();
+		}
 	}
 	
 	/**
@@ -92,6 +104,7 @@ public class ProcessingSketch extends PApplet {
 	private Logger logger;
 	private Recorder recorder;
 	private Renderer renderer;
+	private Replay replay;
 
 	private boolean[] keys = new boolean[526];
 
