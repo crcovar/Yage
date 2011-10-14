ff1791806ab912e0387dad9552e67dbdd180a927
Charles Covar <covar1@gmail.com>
2011-10-13 23:37:02 -0400

    Added empty Replay class.

---
+++ ProcessingEngine/src/engine/utils/Replay.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package engine.utils;
+
+import engine.GameObject;
+
+/**
+ * @author Covar
+ * TODO: Read from replay file
+ * TODO: Push events to renderer
+ * TODO: Support half speed
+ * TODO: Support 2x speed
+ */
+public class Replay extends GameObject {
+	/**
+	 * Default Constructor
+	 */
+	public Replay() {
+		super();
+	}
+
+}
