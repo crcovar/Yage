6c5a5addc623095be0bc13e4361781526168633c
Charles Covar <covar1@gmail.com>
2013-03-05 13:50:25 -0500

    Removes init() from Engine class.

--- src/engine/Engine.java
+++ src/engine/Engine.java
@@ -1,6 +1,7 @@
 package engine;
 
 import org.newdawn.slick.*;
+import org.newdawn.slick.state.*;
 
 import engine.utils.ConfigManager;
 
@@ -22,24 +23,6 @@ public class Engine extends StateBasedGame
     public int getGameState() { return this.gameState; }
     
     @Override
-    public void init(GameContainer gc) throws SlickException {
-        ConfigManager cm = ConfigManager.getInstance();
-           
-        String w = cm.getOption("width");
-        String h = cm.getOption("height");
-    	
-		int width = 640;
-		int height = 480;
-		
-		if(w != null)
-			width = Integer.parseInt(w);
-		if(h != null)
-			height = Integer.parseInt(h);
-        
-        gc.setDisplayMode(width, height, false);
-    }
-    
-    @Override
     public void update(GameContainer gc, int delta) throws SlickException {
         
     }
@@ -50,10 +33,33 @@ public class Engine extends StateBasedGame
     }
     
     public static void main(String[] args) throws SlickException {
+    	ConfigManager cm = ConfigManager.getInstance();
+        
+        String w = cm.getOption("width");
+        String h = cm.getOption("height");
+        String f = cm.getOption("fullscreen");
+    	
+		int width = 640;
+		int height = 480;
+		boolean fullscreen = false;
+		
+		if(w != null)
+			width = Integer.parseInt(w);
+		if(h != null)
+			height = Integer.parseInt(h);
+		if(f != null)
+			fullscreen = Boolean.parseBoolean(f);
         
         AppGameContainer app = new AppGameContainer(new Engine());
+        app.setDisplayMode(width, height, fullscreen);
         app.start();
     }
     
     private static short gameState;
+
+	@Override
+	public void initStatesList(GameContainer arg0) throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
 }
